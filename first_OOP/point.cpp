6b7bc6cb8884627b58e970d66cac6966c9026223
tll471 <brodskyyury@gmail.com>
2025-09-09 16:31:10 +0300

    домашка 09.09.25

--- first_OOP/point.cpp
+++ first_OOP/point.cpp
@@ -1,156 +1,156 @@
-#include <iostream>
-using namespace std;
-
-class Car
-{
-private:
-	char* model = nullptr;
-	char* country = nullptr;
-	char* color = nullptr;
-	int year;
-	double price;
-public:
-	Car()
-	{
-		model = nullptr;
-		country = nullptr;
-		color = nullptr;
-		year = 0;
-		price = 0;
-	}
-	Car(const char* model1, const char* country1, const char* color1, int year1, double price1)
-	{
-		model = new char[strlen(model1) + 1];
-		strcpy_s(model, strlen(model1) + 1, model1);
-
-		country = new char[strlen(country1) + 1];
-		strcpy_s(country, strlen(country1) + 1, country1);
-
-		color = new char[strlen(color1) + 1];
-		strcpy_s(color, strlen(color1) + 1, color1);
-
-		year = year1;
-		price = price1;
-	}
-	void Init(const char* model1, const char* country1, const char* color1, int year1, double price1)
-	{
-		if (model != nullptr)
-		{
-			delete[] model;
-		}
-		model = new char[strlen(model1) + 1];
-		strcpy_s(model, strlen(model1) + 1, model1);
-
-		if (country != nullptr)
-		{
-			delete[] country;
-		}
-		country = new char[strlen(country1) + 1];
-		strcpy_s(country, strlen(country1) + 1, country1);
-
-		if (color != nullptr)
-		{
-			delete[] color;
-		}
-		color = new char[strlen(color1) + 1];
-		strcpy_s(color, strlen(color1) + 1, color1);
-
-		year = year1;
-		price = price1;
-	}
-	char GetModel()
-	{
-		return *model;
-	}
-	char GetCountry()
-	{
-		return *country;
-	}
-	char GetColor()
-	{
-		return *color;
-	}
-	int GetYear()
-	{
-		return year;
-	}
-	double GetPrice()
-	{
-		return price;
-	}
-	void SetYear(int year1)
-	{
-		if (year1 > 0 || year1 < 0)
-		{
-			year = year1;
-		}
-	}
-	void SetPrice(int price1)
-	{
-		if (price1 > 0 || price1 < 0)
-		{
-			price = price1;
-		}
-	}
-
-	void Print()
-	{
-		cout << "Information about car:" << endl;
-		cout << "Model: " << model << endl;
-		cout << "Country: " << country << endl;
-		cout << "Color: " << color << endl;
-		cout << "Year: " << year << endl;
-		cout << "Price: " << price << endl;
-		cout << endl;
-	}
-	~Car()
-	{
-		delete[] model;
-		delete[] country;
-		delete[] color;
-		cout << "Everything is deleted now!" << endl;
-	}
-};
-int main()
-{
-
-	Car obj1;
-	obj1.Init("BMW", "Germany", "Black", 1998, 175.988);
-	obj1.Print();
-
-	Car obj2("Mercedes", "Italy", "White", 2021, 65.123);
-	obj2.Print();
-
-	int size = 20;
-	char* model = new char[size];
-	char* country = new char[size];
-	char* color = new char[size];
-	int year;
-	double price;
-
-	cout << "Enter the next information:" << endl;
-	cout << endl;
-	cout << "Enter car model: " << endl;
-	cin.getline(model, size);
-	cout << "Enter car country: " << endl;
-	cin.getline(country, size);
-	cout << "Enter car color: " << endl;
-	cin.getline(color, size);
-	cout << "Enter car year: " << endl;
-	cin >> year;
-	cout << "Enter can price: " << endl;
-	cin >> price;
-	cout << endl;
-
-	Car obj3(model, country, color, year, price);
-	if (year == 0)
-	{
-		obj3.SetYear(2000);
-	}
-	if (price == 0)
-	{
-		obj3.SetPrice(10000);
-	}
-	obj3.Print();
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//class Car
+//{
+//private:
+//	char* model = nullptr;
+//	char* country = nullptr;
+//	char* color = nullptr;
+//	int year;
+//	double price;
+//public:
+//	Car()
+//	{
+//		model = nullptr;
+//		country = nullptr;
+//		color = nullptr;
+//		year = 0;
+//		price = 0;
+//	}
+//	Car(const char* model1, const char* country1, const char* color1, int year1, double price1)
+//	{
+//		model = new char[strlen(model1) + 1];
+//		strcpy_s(model, strlen(model1) + 1, model1);
+//
+//		country = new char[strlen(country1) + 1];
+//		strcpy_s(country, strlen(country1) + 1, country1);
+//
+//		color = new char[strlen(color1) + 1];
+//		strcpy_s(color, strlen(color1) + 1, color1);
+//
+//		year = year1;
+//		price = price1;
+//	}
+//	void Init(const char* model1, const char* country1, const char* color1, int year1, double price1)
+//	{
+//		if (model != nullptr)
+//		{
+//			delete[] model;
+//		}
+//		model = new char[strlen(model1) + 1];
+//		strcpy_s(model, strlen(model1) + 1, model1);
+//
+//		if (country != nullptr)
+//		{
+//			delete[] country;
+//		}
+//		country = new char[strlen(country1) + 1];
+//		strcpy_s(country, strlen(country1) + 1, country1);
+//
+//		if (color != nullptr)
+//		{
+//			delete[] color;
+//		}
+//		color = new char[strlen(color1) + 1];
+//		strcpy_s(color, strlen(color1) + 1, color1);
+//
+//		year = year1;
+//		price = price1;
+//	}
+//	char GetModel()
+//	{
+//		return *model;
+//	}
+//	char GetCountry()
+//	{
+//		return *country;
+//	}
+//	char GetColor()
+//	{
+//		return *color;
+//	}
+//	int GetYear()
+//	{
+//		return year;
+//	}
+//	double GetPrice()
+//	{
+//		return price;
+//	}
+//	void SetYear(int year1)
+//	{
+//		if (year1 > 0 || year1 < 0)
+//		{
+//			year = year1;
+//		}
+//	}
+//	void SetPrice(int price1)
+//	{
+//		if (price1 > 0 || price1 < 0)
+//		{
+//			price = price1;
+//		}
+//	}
+//
+//	void Print()
+//	{
+//		cout << "Information about car:" << endl;
+//		cout << "Model: " << model << endl;
+//		cout << "Country: " << country << endl;
+//		cout << "Color: " << color << endl;
+//		cout << "Year: " << year << endl;
+//		cout << "Price: " << price << endl;
+//		cout << endl;
+//	}
+//	~Car()
+//	{
+//		delete[] model;
+//		delete[] country;
+//		delete[] color;
+//		cout << "Everything is deleted now!" << endl;
+//	}
+//};
+//int main()
+//{
+//
+//	Car obj1;
+//	obj1.Init("BMW", "Germany", "Black", 1998, 175.988);
+//	obj1.Print();
+//
+//	Car obj2("Mercedes", "Italy", "White", 2021, 65.123);
+//	obj2.Print();
+//
+//	int size = 20;
+//	char* model = new char[size];
+//	char* country = new char[size];
+//	char* color = new char[size];
+//	int year;
+//	double price;
+//
+//	cout << "Enter the next information:" << endl;
+//	cout << endl;
+//	cout << "Enter car model: " << endl;
+//	cin.getline(model, size);
+//	cout << "Enter car country: " << endl;
+//	cin.getline(country, size);
+//	cout << "Enter car color: " << endl;
+//	cin.getline(color, size);
+//	cout << "Enter car year: " << endl;
+//	cin >> year;
+//	cout << "Enter can price: " << endl;
+//	cin >> price;
+//	cout << endl;
+//
+//	Car obj3(model, country, color, year, price);
+//	if (year == 0)
+//	{
+//		obj3.SetYear(2000);
+//	}
+//	if (price == 0)
+//	{
+//		obj3.SetPrice(10000);
+//	}
+//	obj3.Print();
+//
+//}
--- first_OOP/robota.cpp
+++ first_OOP/robota.cpp
@@ -1,155 +1,118 @@
-//#include <iostream>
-//using namespace std;
-//
-//class Drob
-//{
-//private:
-//	int number1;
-//	int number2;
-//	int number3;
-//	int number4;
-//public:
-//	void Init(int n1, int n2, int n3, int n4)
-//	{
-//		number1 = n1;
-//		number2 = n2;
-//		number3 = n3;
-//		number4 = n4;
-//	}
-//	void Print()
-//	{
-//		cout << number1 << "   " << number3 << "\n- + -\n" << number2 << "   " << number4 << endl;;
-//	}
-//	void Print_Plus()
-//	{
-//		cout << "Your answer is: " << endl;
-//		cout << number1 * number4 + number3 * number2 << endl;
-//		cout << "-" << endl;
-//		cout << number2 * number4 << endl;
-//	}
-//	void Print_Minus()
-//	{
-//		cout << "Your answer is: " << endl;
-//		cout << number1 * number4 - number3 * number2 << endl;
-//		cout << "-" << endl;
-//		cout << number2 * number4 << endl;
-//	}
-//	void Print_Multiplication()
-//	{
-//		cout << "Your answer is: " << endl;
-//		cout << number1 * number3 << endl;
-//		cout << "-" << endl;
-//		cout << number2 * number4 << endl;
-//	}
-//	void Print_Division()
-//	{
-//		cout << "Your answer is: " << endl;
-//		cout << number1 * number4 << endl;
-//		cout << "-" << endl;
-//		cout << number2 * number3 << endl;
-//	}
-//	int GetN1() // инспекторы
-//	{
-//		return number1;
-//	}
-//	int GetN2() // инспекторы
-//	{
-//		return number2;
-//	}
-//	int GetN3() // инспекторы
-//	{
-//		return number3;
-//	}
-//	int GetN4() // инспекторы
-//	{
-//		return number4;
-//	}
-//	void SetN1(int n1) // модификаторы
-//	{
-//		number1 = n1;
-//	}
-//	void SetN2(int n2) // модификаторы
-//	{
-//		if (n2 > 0 || n2 < 0) 
-//		{ 
-//			number2 = n2;
-//		}
-//	}
-//	void SetN3(int n3) // модификаторы
-//	{
-//		number3 = n3;
-//	}
-//	void SetN4(int n4) // модификаторы
-//	{
-//		if (n4 > 0 || n4 < 0)
-//		{
-//			number4 = n4;
-//		}
-//	}
-//};
-//
-//int main()
-//{
-//	Drob obj1;
-//	int num1, num2, num3, num4;
-//	cout << "Enter first drob: " << endl;
-//	cout << "Enter first number: " << endl;
-//	cin >> num1;
-//
-//	cout << "Enter second number: " << endl;
-//	cin >> num2;
-//
-//	cout << "Enter second drob: " << endl;
-//	cout << "Enter first number: " << endl;
-//	cin >> num3;
-//
-//	cout << "Enter second number: " << endl;
-//	cin >> num4;
-//
-//	obj1.Init(num1, num2, num3, num4);
-//
-//	if (num2 == 0)
-//	{
-//		obj1.SetN2(1);
-//	}
-//
-//	if (num4 == 0)
-//	{
-//		obj1.SetN4(1);
-//	}
-//	cout << obj1.GetN2() << endl;
-//
-//	int choise;
-//	cout << "What do you want to do? write (1-4)" << endl;
-//	cout << "(1) +" << endl;
-//	cout << "(2) -" << endl;
-//	cout << "(3) *" << endl;
-//	cout << "(4) /" << endl;
-//	cin >> choise;
-//	cout << endl;
-//	switch (choise)
-//	{
-//
-//	case 1:
-//		obj1.Print();
-//		obj1.Print_Plus();
-//		break;
-//	case 2:
-//		obj1.Print();
-//		obj1.Print_Minus();
-//		break;
-//	case 3:
-//		obj1.Print();
-//		obj1.Print_Multiplication();
-//		break;
-//	case 4:
-//		obj1.Print();
-//		obj1.Print_Division();
-//		break;
-//
-//	default:
-//		cout << "Please Try again!" << endl;
-//		break;
-//	}
-//
-//}
+#include <iostream>
+using namespace std;
+
+class Drob
+{
+private:
+	int num;
+	int den;
+public:
+	void Init(int n1, int n2)
+	{
+		this->num = n1;
+		this->den = n2;
+	}
+	void Print()
+	{
+		cout << this->num << "/" << this->den << endl;
+	}
+	Drob Plus(Drob& obj2)
+	{
+		Drob answer1;
+		answer1.num = this->num * obj2.den + obj2.num * this->den;
+		answer1.den = this->den * obj2.den;
+		return answer1;
+	}
+
+	Drob Minus(Drob& obj2)
+	{
+		Drob answer1;
+		answer1.num = this->num * obj2.den - obj2.num * this->den;
+		answer1.den = this->den * obj2.den;
+		return answer1;
+	}
+
+	Drob Multiplication(Drob& obj2) 
+	{
+		Drob answer1;
+		answer1.num = this->num * obj2.num;
+		answer1.den = this->den * obj2.den;
+		return answer1;
+	}
+
+	Drob Division(Drob& obj2)
+	{
+		Drob answer1;
+		answer1.num = this->num * obj2.den;
+		answer1.den = this->den * obj2.num;
+		return answer1;
+	}
+	Drob& SetNum(int num) 
+	{
+		this->num = num;
+		return *this;
+	}
+	Drob& SetDen(int den)
+	{
+		this->den = den;
+		return *this;
+	}
+	
+};
+
+int main()
+{
+
+	Drob obj1, obj2, obj3;
+	int num1, num2;
+	cout << "Enter first number: " << endl;
+	cin >> num1;
+
+	cout << "Enter second number: " << endl;
+	cin >> num2;
+
+	obj1.Init(num1, num2);
+	
+	cout << "Enter first number: " << endl;
+	cin >> num1;
+
+	cout << "Enter second number: " << endl;
+	cin >> num2;
+
+	obj2.Init(num1, num2);
+
+
+
+	int choise;
+	cout << "What do you want to do? write (1-4)" << endl;
+	cout << "(1) +" << endl;
+	cout << "(2) -" << endl;
+	cout << "(3) *" << endl;
+	cout << "(4) /" << endl;
+	cin >> choise;
+	cout << endl;
+
+	switch (choise)
+	{
+
+	case 1:
+		obj3 = obj1.Plus(obj2);
+		break;
+	case 2:
+		obj3 = obj1.Minus(obj2);
+		break;
+	case 3:
+		obj3 = obj1.Multiplication(obj2);
+		break;
+	case 4:
+		obj3 = obj1.Division(obj2);
+		break;
+
+	default:
+		cout << "Please Try again!" << endl;
+		break;
+	}
+	cout << "Answer: " << endl;
+	obj3.Print();
+
+}
